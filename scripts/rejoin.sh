631d188fd801057566cacd33cbf37d7333e5f9e8
Faizal Sadri <faizal@geekdo.me>
2021-04-15 15:52:24 +0800

    Update rejoin script

--- scripts/rejoin.sh
+++ scripts/rejoin.sh
@@ -4,6 +4,6 @@ CMD="$(dirname $0)/$(basename $0)"
 if [ $# -ne 1 ]; then
     echo "${CMD} <host/ip>"
 else
-    repmgr node rejoin -d "postgres://repmgr@${1}:5432/repmgr" --force-rewind
+    repmgr -f /etc/repmgr.conf node rejoin -d "postgres://repmgr@${1}:5432/repmgr" --force-rewind
 fi
 
